678698893efad633d4f4b6eaf44c1720c6cb7ef8
imanoj997 <mbhatt997@gmail.com>
2018-11-29 08:49:16 +0545

    slider with buttons

--- Assignment 1/script.js
+++ Assignment 1/script.js
@@ -1,40 +1,174 @@
-var imageNumber = 5;
-
-var slider = document.getElementById('slider');
-slider.style.position = 'absolute';
-
-var images = document.getElementsByClassName('image');
-
-
-var width = images[0].width;
-var widthCount = 0;
-var left = images[0].width/500;
-var totalWidth = 0;
+var ul;
+var liItems;
+var imageWidth;
+var imageNumber;
+var currentImage=0;
 
 
+ul = document.getElementById('image_slider');
 
-var interval = setInterval(slide ,4);
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
 
-function slide() {
-  if (widthCount == 0) {
-    slider.style.left = - left + 'px';
-    widthCount = left;
-  }
-  else {
-    if (widthCount<900) {
-      slider.style.left = - (left + widthCount) + 'px';
-      widthCount = (left + widthCount);
-    } else {
-      totalWidth=widthCount;
-      clearInterval(interval);
+init();
 
-      
-    }
-  }
+function init() {
+
+    liItems = ul.children;
+    imageNumber = liItems.length;
+    imageWidth = liItems[0].children[0].clientWidth;
+    ul.style.width = parseInt(imageWidth * imageNumber) + 'px';
+    prev.onclick = function () {
+        onClickPrev();
+    };
+    next.onclick = function () {
+        onClickNext();
+    };
+}
+
+function slideTo(imageToGo) {
+    var direction;
+    var numOfImageToGo = Math.abs(imageToGo - currentImage);
+    direction = currentImage > imageToGo ? 1 : -1;
+    currentPosition = -1 * currentImage * imageWidth;
+    var opts = {
+        duration: 1000,
+        delta: function (p) {
+            return p;
+        },
+        step: function (delta) {
+            ul.style.left = parseInt(currentPosition + direction * delta * imageWidth * numOfImageToGo) + 'px';
+        },
+        callback: function () {
+            currentImage = imageToGo;
+        }
+    };
+    animate(opts);
+}
+
+function goBack(leftPosition) {
+    currentImage = 0;
+    var id = setInterval(function () {
+        if (leftPosition >= 0) {
+            ul.style.left = '-' + parseInt(leftPosition) + 'px';
+            leftPosition -= imageWidth / 10;
+        } else {
+            clearInterval(id);
+        }
+    }, 17);
+}
+
+function animate(opts) {
+    var start = new Date();
+    var id = setInterval(function () {
+        var timePassed = new Date() - start;
+        var progress = timePassed / opts.duration;
+        if (progress > 1) {
+            progress = 1;
+        }
+        var delta = opts.delta(progress);
+        opts.step(delta);
+        if (progress == 1) {
+            clearInterval(id);
+            opts.callback();
+        }
+    }, opts.delay || 17);
 }
 
 
 
+function onClickPrev() {
+    if (currentImage == 0) {
+        slideTo(imageNumber - 1);
+    } else {
+        slideTo(currentImage - 1);
+    }
+}
+
+function onClickNext() {
+    if (currentImage == imageNumber - 1) {
+        slideTo(0);
+    } else {
+        slideTo(currentImage + 1);
+    }
+}
+
+function generatePager(imageNumber) {
+    var pageNumber;
+    var pagerDiv = document.getElementById('pager');
+    for (i = 0; i < imageNumber; i++) {
+        var li = document.createElement('li');
+        pageNumber = document.createTextNode(parseInt(i + 1));
+        li.appendChild(pageNumber);
+        pagerDiv.appendChild(li);
+
+        li.onclick = function (i) {
+            return function () {
+                slideTo(i);
+            }
+        }(i);
+    }
+
+    var computedStyle = document.defaultView.getComputedStyle(li, null);
+
+    var liWidth = li.offsetWidth;
+
+    var liMargin = parseInt(computedStyle.margin.replace('px', ""));
+
+    pagerDiv.style.width = parseInt((liWidth + liMargin * 2) * imageNumber) + 'px';
+}
+
+
+
+
+// function animate(optional) {
+//     var start = new Date();
+//     var id = setInterval(function () {
+//         var timePassed = new Date() - start;
+//         var progress = timePassed / optional.duration;
+//         if (progress > 1) {
+//             progress = 1;
+//         }
+//         var delta = optional.delta(progress);
+//         optional.step(delta);
+//         if (progress == 1) {
+//             clearInterval(id);
+//             optional.callback();
+//         }
+//     }, optional.dalay || 17);
+// }
+
+
+// function slider(ul) {
+//     animate({
+//         delay: 17,
+//         duration: 3000,
+//         delta: function (p) {
+//             return Math.max(0, -1 + 2 * p);
+//         },
+//         step: function (delta) {
+//             ul.style.left = '-' + parseInt(currentImage * imageWidth + delta * imageWidth) + 'px';
+//         },
+//         callback: function () {
+//             currentImage++;
+//             // if it doesn’t slied to the last image, keep sliding
+//             if (currentImage < imageNumber - 1) {
+//                 slider(ul);
+//             }
+//             // if current image it’s the last one, it slides back to the first one
+//             else {
+//                 var leftPosition = (imageNumber - 1) * imageWidth;
+//                 // after 2 seconds, call the goBack function to slide to the first image 
+//                 setTimeout(function () {
+//                     goBack(leftPosition);
+//                 }, 3000);
+//                 setTimeout(function () {
+//                     slider(ul);
+//                 }, 4000);
+//             }
+//         }
+//     });
+// }
 
 
 
